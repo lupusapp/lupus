e357a271c715fdda5228041cb9b104817fab828b
Ogromny <ogromnycoding@gmail.com>
2019-07-15 15:14:30 +0200

    feat(LupusProfile): add SAVE signal, tox props, callback for name change and status_message change

--- include/lupus_profile.h
+++ include/lupus_profile.h
@@ -2,11 +2,12 @@
 #define LUPUS_LUPUS_PROFILE_H
 
 #include <gtk/gtk.h>
+#include "toxcore/tox.h"
 
 #define LUPUS_TYPE_PROFILE lupus_profile_get_type()
 
-G_DECLARE_FINAL_TYPE(LupusProfile, lupus_profile, LUPUS, PROFILE, GtkGrid)
+G_DECLARE_FINAL_TYPE(LupusProfile, lupus_profile, LUPUS, PROFILE, GtkBox)
 
-LupusProfile *lupus_profile_new(void);
+LupusProfile *lupus_profile_new(Tox *tox);
 
 #endif
