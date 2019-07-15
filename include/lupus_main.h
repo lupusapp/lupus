b12a5f8dbe4bc2e831af296a8803312a491a8804
Ogromny <ogromnycoding@gmail.com>
2019-07-15 15:12:31 +0200

    feat(LupusMain): add SAVE signal and profile-{filename,password} props

--- include/lupus_main.h
+++ include/lupus_main.h
@@ -5,11 +5,12 @@
 #include "toxcore/tox.h"
 #include "lupus_headerbar.h"
 #include "lupus_profile.h"
+#include "lupus.h"
 
 #define LUPUS_TYPE_MAIN lupus_main_get_type()
 
 G_DECLARE_FINAL_TYPE(LupusMain, lupus_main, LUPUS, MAIN, GtkApplicationWindow)
 
-LupusMain *lupus_main_new(GtkApplication *application, Tox *tox);
+LupusMain *lupus_main_new(GtkApplication *application, Tox *tox, gchar *profile_filename, gchar *profile_password);
 
 #endif
--- src/lupus_main.c
+++ src/lupus_main.c
@@ -1,4 +1,6 @@
 #include "../include/lupus_main.h"
+#include "../include/lupus_editable_label.h"
+#include "../include/utils.h"
 
 struct _LupusMain {
     GtkApplicationWindow parent_instance;
@@ -7,12 +9,16 @@ struct _LupusMain {
 typedef struct _LupusMainPrivate LupusMainPrivate;
 struct _LupusMainPrivate {
     Tox *tox;
+    gchar *profile_name;
+    gchar *profile_password;
 };
 
 G_DEFINE_TYPE_WITH_PRIVATE(LupusMain, lupus_main, GTK_TYPE_APPLICATION_WINDOW)
 
 enum {
     PROP_TOX = 1,
+    PROP_PROFILE_NAME,
+    PROP_PROFILE_PASSWORD,
     N_PROPERTIES
 };
 
@@ -26,6 +32,12 @@ static void lupus_main_set_property(GObject *object, guint property_id, GValue c
             //TODO: handle reset
             priv->tox = g_value_peek_pointer(value);
             break;
+        case PROP_PROFILE_NAME:
+            priv->profile_name = g_value_dup_string(value);
+            break;
+        case PROP_PROFILE_PASSWORD:
+            priv->profile_password = g_value_dup_string(value);
+            break;
         default:
             G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
     }
@@ -38,43 +50,75 @@ static void lupus_main_get_property(GObject *object, guint property_id, GValue *
         case PROP_TOX:
             g_value_set_pointer(value, priv->tox);
             break;
+        case PROP_PROFILE_NAME:
+            g_value_set_string(value, priv->profile_name);
+            break;
+        case PROP_PROFILE_PASSWORD:
+            g_value_set_string(value, priv->profile_password);
+            break;
         default:
             G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
     }
 }
 
-static void lupus_main_init(LupusMain *instance) {
-    LupusHeaderbar *header_bar = lupus_headerbar_new();
-    gtk_window_set_titlebar(GTK_WINDOW(instance), GTK_WIDGET(header_bar));
-
-    LupusProfile *profile = lupus_profile_new();
-    GtkWidget *grid = gtk_grid_new();
-    gtk_grid_attach(GTK_GRID(grid), GTK_WIDGET(profile), 0, 0, 1, 1);
-
-    gtk_container_add(GTK_CONTAINER(instance), grid);
-
-    gtk_widget_show_all(GTK_WIDGET(instance));
+static void save_callback(LupusProfile *profile, gpointer user_data) {
+    LupusMainPrivate *priv = lupus_main_get_instance_private(LUPUS_MAIN(user_data));
+    save_tox(priv->tox, priv->profile_name, priv->profile_password, user_data);
 }
 
-static void lupus_main_class_init(LupusMainClass *klass) {
-    GObjectClass *object_class = G_OBJECT_CLASS(klass);
+static void lupus_main_constructed(GObject *object) {
+    LupusMainPrivate *priv = lupus_main_get_instance_private(LUPUS_MAIN(object));
+
+    LupusProfile *profile = lupus_profile_new(priv->tox);
+    g_signal_connect(profile, "save", G_CALLBACK(save_callback), object);
+
+    GtkWidget *separator = gtk_separator_new(GTK_ORIENTATION_HORIZONTAL);
+
+    GtkWidget *grid = gtk_grid_new();
+    gtk_grid_attach(GTK_GRID(grid), GTK_WIDGET(profile), 0, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), GTK_WIDGET(separator), 0, 1, 1, 1);
+
+    gtk_container_add(GTK_CONTAINER(object), grid);
+    gtk_widget_show_all(grid);
+}
+
+static void lupus_main_init(LupusMain *instance) {
+    gtk_window_set_titlebar(GTK_WINDOW(instance), GTK_WIDGET(lupus_headerbar_new()));
+}
+
+static void lupus_main_class_init(LupusMainClass *class) {
+    GObjectClass *object_class = G_OBJECT_CLASS(class);
 
     object_class->set_property = lupus_main_set_property;
     object_class->get_property = lupus_main_get_property;
+    object_class->constructed = lupus_main_constructed;
 
     obj_properties[PROP_TOX] = g_param_spec_pointer(
             "tox",
             "Tox",
             "Tox instance to use.",
-            G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE
-    );
+            G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY);
+    obj_properties[PROP_PROFILE_NAME] = g_param_spec_string(
+            "profile-filename",
+            "profile_filename",
+            "Profile's filename",
+            "UNKNOWN",
+            G_PARAM_READWRITE);
+    obj_properties[PROP_PROFILE_PASSWORD] = g_param_spec_string(
+            "profile-password",
+            "profile_password",
+            "Profile's password",
+            "UNKNOWN",
+            G_PARAM_READWRITE);
 
     g_object_class_install_properties(object_class, N_PROPERTIES, obj_properties);
 }
 
-LupusMain *lupus_main_new(GtkApplication *application, Tox *tox) {
+LupusMain *lupus_main_new(GtkApplication *application, Tox *tox, gchar *profile_filename, gchar *profile_password) {
     return g_object_new(LUPUS_TYPE_MAIN,
                         "application", application,
                         "tox", tox,
+                        "profile-filename", profile_filename,
+                        "profile-password", profile_password,
                         NULL);
 }
