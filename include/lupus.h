2391d21d185750b6bd2375b3c581b1be8747f374
Ogromny <ogromnycoding@gmail.com>
2020-03-07 16:17:42 +0100

    lupus.h: rewrite macro

--- include/lupus.h
+++ include/lupus.h
@@ -5,24 +5,17 @@
 #define LUPUS_RESOURCES "/ru/ogromny/lupus"
 
 #define lupus_error(i, ...)                                                    \
-    ({                                                                         \
-        GtkWidget *d = gtk_message_dialog_new(                                 \
-            GTK_WINDOW(i), GTK_DIALOG_USE_HEADER_BAR, GTK_MESSAGE_ERROR,       \
-            GTK_BUTTONS_CLOSE, NULL);                                          \
-        gtk_message_dialog_set_markup(GTK_MESSAGE_DIALOG(d), "<b>Error</b>");  \
-        gtk_message_dialog_format_secondary_markup(GTK_MESSAGE_DIALOG(d),      \
-                                                   __VA_ARGS__);               \
-        gtk_dialog_run(GTK_DIALOG(d));                                         \
-        gtk_widget_destroy(GTK_WIDGET(d));                                     \
-    })
+    (lupus_message(GTK_MESSAGE_ERROR, "<b>Error</b>", i, __VA_ARGS__))
 
 #define lupus_success(i, ...)                                                  \
+    (lupus_message(GTK_MESSAGE_INFO, "<b>Success</b>", i, __VA_ARGS__))
+
+#define lupus_message(t, s, i, ...)                                            \
     ({                                                                         \
-        GtkWidget *d = gtk_message_dialog_new(                                 \
-            GTK_WINDOW(i), GTK_DIALOG_USE_HEADER_BAR, GTK_MESSAGE_ERROR,       \
-            GTK_BUTTONS_CLOSE, NULL);                                          \
-        gtk_message_dialog_set_markup(GTK_MESSAGE_DIALOG(d),                   \
-                                      "<b>Success</b>");                       \
+        GtkWidget *d =                                                         \
+            gtk_message_dialog_new(GTK_WINDOW(i), GTK_DIALOG_USE_HEADER_BAR,   \
+                                   t, GTK_BUTTONS_CLOSE, NULL);                \
+        gtk_message_dialog_set_markup(GTK_MESSAGE_DIALOG(d), s);               \
         gtk_message_dialog_format_secondary_markup(GTK_MESSAGE_DIALOG(d),      \
                                                    __VA_ARGS__);               \
         gtk_dialog_run(GTK_DIALOG(d));                                         \
