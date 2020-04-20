a574db9147529fcdd970bd8b0909942cd16ac309
Ogromny <ogromnycoding@gmail.com>
2020-04-20 23:52:17 +0200

    LupusMainChat: add some basic features

--- include/lupus_mainchat.h
+++ include/lupus_mainchat.h
@@ -1,6 +1,7 @@
 #ifndef __LUPUS_LUPUS_MAINCHAT_H__
 #define __LUPUS_LUPUS_MAINCHAT_H__
 
+#include "lupus_wrapperfriend.h"
 #include <gtk/gtk.h>
 
 #define LUPUS_TYPE_MAINCHAT lupus_mainchat_get_type()
@@ -8,5 +9,6 @@
 G_DECLARE_FINAL_TYPE(LupusMainChat, lupus_mainchat, LUPUS, MAINCHAT, GtkEventBox)
 
 LupusMainChat *lupus_mainchat_new(void);
+void lupus_mainchat_add_message(LupusMainChat *instance, LupusWrapperFriend *friend, gchar const *text);
 
 #endif
